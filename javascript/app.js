02539a3124dca4577cf3d2b16c4fee317ab0f665
Dev-JohnKennedy <john.jk@hotmail.com>
2024-09-04 11:33:57 -0300

    Novo Arquivo JS.
    Foi criado novo arquivo Dados.

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        nomde: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e um verdadeiro orgulho nacional. Nascida em Guarulhos, São Paulo, em 1999, ela conquistou o coração dos brasileiros com sua determinação, talento e superação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        nome: "Beatriz Souza",
+        descricao: "Beatriz Souza é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e um verdadeiro orgulho nacional. Nascida em Guarulhos, São Paulo, em 1999, ela conquistou o coração dos brasileiros com sua determinação, talento e superação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        nome: "Rayssa Leal",
+        descricao: "Rayssa Leal é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e um verdadeiro orgulho nacional. Nascida em Guarulhos, São Paulo, em 1999, ela conquistou o coração dos brasileiros com sua determinação, talento e superação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+]
