cef19d369993a2b886222d488775b4f10e8666ae
Dev-JohnKennedy <john.jk@hotmail.com>
2024-09-06 15:19:24 -0300

    Alterações
    Alterações do style, dados e app.

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,43 @@
-let section = document.getElementById(resultados-pesquisa)
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar a marca ou modelo do veiculo</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let marca = ""; 
+    let modelo = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        marca = dado.marca.toLowerCase()
+        modelo = dado.modelo.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (marca.includes(campoPesquisa) || modelo.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+                <div class="resultado">
+                    <h2>${marca} ${modelo}</h2>
+                    <p>Versão: ${dado.vercao}</p>
+                    <p>Descrição: ${dado.descricao}</p>
+                </div>
+            `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
--- javascript/dados.js
+++ javascript/dados.js
@@ -1,17 +1,39 @@
 let dados = [
     {
-        nomde: "Rebeca Andrade",
-        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e um verdadeiro orgulho nacional. Nascida em Guarulhos, São Paulo, em 1999, ela conquistou o coração dos brasileiros com sua determinação, talento e superação.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        marca: "Fiat",
+        modelo: "Argo",
+        vercao: "Argo 1.0 Manual",
+        descricao: "A combinação perfeita entre custo benefício com o máximo de estilo.\nAr-Condicionado | Direção Elétrica | Alarme com chave canivete",
+        cores: "Preto, Branco, Vermelho, Cinza ou Prata.",
+        valor: "A partir de R$ 85.990,00.",
+        link: "https://argo.fiat.com.br/versoes"
     },
     {
-        nome: "Beatriz Souza",
-        descricao: "Beatriz Souza é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e um verdadeiro orgulho nacional. Nascida em Guarulhos, São Paulo, em 1999, ela conquistou o coração dos brasileiros com sua determinação, talento e superação.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        marca: "Fiat",
+        modelo: "Argo",
+        vercao: "Drive 1.0 Manual",
+        descricao: "Personalidade, conforto e tecnologia andam juntos com o Argo!\nSensor de pressão nos pneus | Comandos no volante | Central Multimídia 7” | Ar-Condicionado | Direção Elétrica | Alarme com chave canivete",
+        consumo: "Consumo médio: Gasolina = 14,2 / Etanol 9,9 Cidade (km/l) e 15,1 Estrada (km/l).",
+        cores: "Preto, Branco, Vermelho, Cinza ou Prata.",
+        valor: "A partir de R$ 87.990,00.",
+        link: "https://argo.fiat.com.br/versoes/drive-1.0"
     },
     {
-        nome: "Rayssa Leal",
-        descricao: "Rayssa Leal é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e um verdadeiro orgulho nacional. Nascida em Guarulhos, São Paulo, em 1999, ela conquistou o coração dos brasileiros com sua determinação, talento e superação.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        marca: "Fiat",
+        modelo: "Argo",
+        vercao: "Endurance 1.3 Manual",
+        descricao: "Feito para acompanhar o seu negócio em qualquer desafio.\nSuspensão elevada | Motor 1.3 Firefly 107 cv | Pneus de uso misto | Protetor de Cárter | Sensor de pressão nos pneus | Comandos no volante | Central Multimídia 7” | Ar-Condicionado | Direção Elétrica | Alarme com chave canivete",
+        cores: "Preto, Branco, Vermelho, Cinza ou Prata.",
+        valor: "A partir de R$ 89.990,00.",
+        link: "https://argo.fiat.com.br/versoes/endurance-1.3"
+    },
+    {
+        marca: "Fiat",
+        modelo: "Argo",
+        vercao: "Drive 1.3 Automático",
+        descricao: "Feito para acompanhar o seu negócio em qualquer desafio.\nCâmbio automático CVT | Led Design | Suspensão elevada | Motor 1.3 Firefly 107 cv | Pneus de uso misto | Protetor de Cárter | Sensor de pressão nos pneus | Comandos no volante | Central Multimídia 7” | Ar-Condicionado Digital | Direção Elétrica | Alarme com chave canivete",
+        cores: "Preto, Branco, Vermelho, Cinza ou Prata.",
+        valor: "A partir de R$ 97.990,00.",
+        link: "https://argo.fiat.com.br/versoes/drive-1.3-at"
     }
 ]
